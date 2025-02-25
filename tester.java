76daaa53c5b53c823d2091f19d51ab1454584cce
Andoud123 <andyjoehaddad@hotmail.com>
2025-02-25 23:48:01 +0200

    Add files via upload

--- tester.java
+++ tester.java
@@ -9,13 +9,13 @@ public class tester {
         TaskManager taskManager = new TaskManager(user.getUsername(), new ArrayList<>());
         RepeatedTask task1 = new RepeatedTask("OOD", "Object Oriented Design", LocalDateTime.of(2025,01,8,10,0,0),LocalDateTime.of(2025,01,8,11,0,0),3);
         DeadlineTask task2 = new DeadlineTask("OOP", "Object Oriented Programming", LocalDateTime.of(2026,01,13,10,0,0), LocalDateTime.of(2026,01,14,10,0,0),LocalTime.of(1, 0),2);
-        taskManager.addTask(task1);
-        taskManager.addTask(task2);
+        System.out.println(taskManager.addTask(task1));
+        System.out.println(taskManager.addTask(task2));
         taskManager.displayTasksForWeek(LocalDate.of(2025,01,9));  
         task2.displayTaskDetails();
         task1.displayTaskDetails();
         System.out.println(task2.timeUntilStart());
-        taskManager.removeTask(task1);
-        taskManager.addTask(null);
+        System.out.println(taskManager.removeTask(task1));
+        System.out.println(taskManager.addTask(null));
     }
 }
